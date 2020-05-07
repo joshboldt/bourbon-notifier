8f085ccd46ab3848975f4e6c771bb68216411211
joshboldt <jboldt12@gmail.com>
2020-05-07 00:13:18 -0400

    a completely different test

--- api/bourbon_notifier.py
+++ api/bourbon_notifier.py
@@ -1,15 +1,9 @@
-from http.server import BaseHTTPRequestHandler
-from datetime import datetime
+from bottle import Bottle, request
 
-from ohlq import main as bourbon_notifier
+from ohlq import main
 
-class handler(BaseHTTPRequestHandler):
-
-  def do_GET(self):
-    self.send_response(200)
-    self.send_header('Content-type', 'text/plain')
-    self.end_headers()
-    result = bourbon_notifier()
-    print(result)
-    # self.wfile.write(str(result).encode())
-    return
+@app.get('/api')
+def api():
+    domain = request.query.get('domain')
+    result = main()
+    return dict(data=result)
